fce8349a907f4642fa4b867b0417e9bcfa956793
Ltdowl <sashag12s@gmail.com>
2022-04-17 10:01:55 +0300

    attempt

---
+++ src/js/service-api.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+export default class ServiceAPI {
+  constructor() {
+    this.options = {
+      params: {
+        key: '26803682-7d038b411c0c8c8df438e6d20',
+        q: '',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: 1,
+        per_page: 40,
+      },
+    };
+  }
+
+  async getPictures() {
+    const response = await axios.get('https://pixabay.com/api/', this.options);
+    this.incrementPage();
+    return response;
+  }
+
+  incrementPage() {
+    this.options.params.page += 1;
+  }
+
+  resetPage() {
+    this.pageNumber = 1;
+  }
+
+  get searchQuery() {
+    return this.options.params.q;
+  }
+
+  set searchQuery(newQuery) {
+    this.options.params.q = newQuery;
+  }
+
+  get pageNumber() {
+    return this.options.params.page;
+  }
+
+  set pageNumber(newNumber) {
+    this.options.params.page = newNumber;
+  }
+}
